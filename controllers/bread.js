d3094c4c7f26ae9ead40850c792800453b4cd5a7
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-05-01 18:56:09 -0400

    Adding routes table to README

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
-const Bread = require("../models/bread");
+const router = require('express').Router();
+const Bread = require('../models/bread');
 
 //get all the bread
-router.get("/", (req, res) => {
-  res.send(Bread);
+router.get('/', (req, res) => {
+  res.render('index', {
+    breads: Bread,
+  });
 });
 
 //get bread by
-router.get("/:index", (req, res) => {
+router.get('/:index', (req, res) => {
   const { index } = req.params;
   res.send(Bread[index]);
 });
